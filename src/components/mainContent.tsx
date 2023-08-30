49e3d34c360d75879646e98ee7f89beaf6ec16ce
Trifall <JerrenT@Trifall.com>
2023-08-30 02:08:13 -0400

    add flex and padding for cards

--- src/components/mainContent.tsx
+++ src/components/mainContent.tsx
@@ -6,10 +6,12 @@ const MainContent = () => {
 	const { result, from_unit, to_unit, inputValue } = useCalculationStore();
 
 	return (
-		<div className='flex flex-row'>
+		<div className='flex flex-col gap-4 md:flex-row'>
 			<ConvertForm />
 			{result !== undefined && from_unit && to_unit && (
-				<ResultCard inputValue={inputValue} result={result} from_unit={from_unit} to_unit={to_unit} />
+				<div className='px-5'>
+					<ResultCard inputValue={inputValue} result={result} from_unit={from_unit} to_unit={to_unit} />
+				</div>
 			)}
 		</div>
 	);
