21deb956dfb11519a2290348584143f70784c4ab
Trifall <JerrenT@Trifall.com>
2023-08-30 05:24:22 -0400

    Set darkmode theme

---
+++ src/styles/themeContext.tsx
@@ -0,0 +1,42 @@
+import { ReactNode, createContext, useCallback, useContext, useEffect, useState } from 'react';
+
+interface IThemeContext {
+	theme: string;
+	toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<IThemeContext>({
+	theme: 'dark',
+	toggleTheme: () => console.warn('no theme provider'),
+} as IThemeContext);
+
+interface ThemeProviderProps {
+	children: ReactNode;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+	const [theme, setTheme] = useState<string>('dark');
+
+	const toggleTheme = useCallback(() => {
+		const toggleTheme = theme === 'dark' ? 'light' : 'dark';
+		setTheme(toggleTheme);
+		window.localStorage.setItem('theme', toggleTheme);
+	}, [theme]);
+
+	useEffect(() => {
+		const localTheme = window.localStorage.getItem('theme');
+		localTheme && setTheme(localTheme);
+	}, []);
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+
+	return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
+
+export const useTheme = () => useContext<IThemeContext>(ThemeContext);
