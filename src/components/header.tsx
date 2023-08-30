4305ff6f8738b35c8621a2c0f1dd5eab04d6813f
Trifall <JerrenT@Trifall.com>
2023-08-30 05:10:03 -0400

    Add description to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,12 +8,17 @@ type HeaderProps = {
 const Header = ({ firstUnit, secondUnit }: HeaderProps) => {
 	return (
 		<section className='body-font text-gray-600'>
-			<div className='container mx-auto flex flex-col px-5 py-6 md:flex-row'>
+			<div className='container mx-auto flex flex-col px-5 py-6 '>
 				<div className='mb-6 flex w-full flex-col pr-0 text-center md:mb-0 md:w-auto md:pr-10 md:text-left'>
 					<h1 className='title-font text-2xl font-medium text-gray-900 md:text-3xl'>
 						Converting {HeaderConvert(firstUnit, secondUnit)}
 					</h1>
 					<h2 className='title-font mb-1 text-lg font-medium tracking-widest text-indigo-500'>Time Converter</h2>
+				</div>
+				<div>
+					<h3 className='title-font mb-1 text-center text-sm tracking-widest text-gray-300 md:text-start'>
+						Select a time unit to convert from and a time unit to convert to, and enter a time value to convert.
+					</h3>
 				</div>
 			</div>
 		</section>
