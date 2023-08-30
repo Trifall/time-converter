cb3725b9ab1f0bef85ea6adfdd7f160e6180b340
Trifall <JerrenT@Trifall.com>
2023-08-30 02:07:35 -0400

    fix footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className='absolute bottom-2 text-gray-300'>
+		<footer className='absolute bottom-2 w-full text-center text-gray-300'>
 			© {new Date().getFullYear()} <a href='https://trifall.com'>Jerren Trifan</a>
 		</footer>
 	);
